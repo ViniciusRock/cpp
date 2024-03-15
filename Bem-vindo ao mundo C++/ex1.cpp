21698a96d4be600c4f0b3a45aaaca0cf149438f0
Vinicius Rocha <37674676+ViniciusRock@users.noreply.github.com>
2024-03-15 03:18:32 -0300

    Add files via upload
    /*
    * Criar um programa que exiba as informações abaixo:
    Bem-vindo
    ao mundo
    do C++
    >Usando apenas um cout. 
    >Para fazer isso, crie uma versão que utilize
     apenas endl para saltar linhas
    */
    /*

---
+++ Bem-vindo ao mundo C++/ex1.cpp
@@ -0,0 +1,18 @@
+/*
+* Criar um programa que exiba as informações abaixo:
+Bem-vindo
+ao mundo
+do C++
+>Usando apenas um cout. 
+>Para fazer isso, crie uma versão que utilize
+ apenas endl para saltar linhas
+*/
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Bem-vindo" << endl << "ao mundo" << endl << "do C++";
+	return 0;
+}
+*/
